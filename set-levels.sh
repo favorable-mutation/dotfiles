b2348b781075e390d574bb6a943519f916e6ce86
Griffin J Rademacher <rademacher.gr@husky.neu.edu>
2018-11-09 15:40:07 -0500

    Added confirmation messages to scripts.

--- set-levels.sh
+++ set-levels.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-# set the system volume to about 1/3 max
-osascript -e "set Volume 2.2"
+# prompt the user to run the script at their own risk
+read -p "If you run this script, all open applications will quit to force recognition of the new settings. Continue? (y/n) " choice
 
-# toggle off system UI noises
-defaults write -globalDomain "com.apple.sound.uiaudio.enabled" -integer 0
+# based on the user's choice, run the script or don't
+case "$choice" in
+    
+   [yY]|[yY][eE]*[sS]* )
 
-# toggle off system error noises
-defaults write -globalDomain "com.apple.sound.beep.feedback" -integer 0
+        # set the system volume to about 1/3 max
+        osascript -e "set Volume 2.2"
 
-# set system error noise volume to min 
-defaults write -globalDomain "com.apple.sound.beep.volume" -float 0
+        # toggle off system UI noises
+        defaults write -globalDomain "com.apple.sound.uiaudio.enabled" -integer 0
 
-# print a comforting feedback message
-echo "System sounds have been turned off"
+        # toggle off system error noises
+        defaults write -globalDomain "com.apple.sound.beep.feedback" -integer 0
+
+        # set system error noise volume to min 
+        defaults write -globalDomain "com.apple.sound.beep.volume" -float 0
+
+        # print a comforting feedback message
+        echo "System sounds have been turned off."
+
+        ;;
+
+    [nN]|[nN][oO]* )
+
+        # print a comforting feedback message
+        echo "Okay. The script will not be run.";;
+
+    * )
+
+        # print a less comforting feedback message
+        echo "Sorry, I didn't get that.";;
+
+esac
--- unset-levels.sh
+++ unset-levels.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-# toggle on system UI noises
-defaults write -globalDomain "com.apple.sound.uiaudio.enabled" -integer 1
+# prompt the user to run the script at their own risk
+read -p "If you run this script, all open applications will quit to force recognition of the new settings. Continue? (y/n) " choice
 
-# toggle on system error noises
-defaults write -globalDomain "com.apple.sound.beep.feedback" -integer 1
+# based on the user's choice, run the script or don't
+case "$choice" in
+    
+   [yY]|[yY][eE]*[sS]* )
 
-# set system error noise volume to max 
-defaults write -globalDomain "com.apple.sound.beep.volume" -float 10
+        # toggle on system UI noises
+        defaults write -globalDomain "com.apple.sound.uiaudio.enabled" -integer 1
 
-# print a comforting feedback message
-echo "System sounds have been turned on"
+        # toggle on system error noises
+        defaults write -globalDomain "com.apple.sound.beep.feedback" -integer 1
+
+        # set system error noise volume to max 
+        defaults write -globalDomain "com.apple.sound.beep.volume" -float 10
+
+        # print a comforting feedback message
+        echo "System sounds have been turned on."
+
+        ;;
+
+    [nN]|[nN][oO]* )
+
+        # print a comforting feedback message
+        echo "Okay. The script will not be run.";;
+
+    * )
+
+        # print a less comforting feedback message
+        echo "Sorry, I didn't get that.";;
+
+esac
